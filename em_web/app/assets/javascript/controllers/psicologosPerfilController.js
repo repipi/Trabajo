57564dcd27a98d4dfa26549d3c0c0c7c95e671fa
repipi <raquelgilmartinez@gmail.com>
2017-12-19 17:27:45 -0100

    Se eliminan las carpetas innecesarias

--- em_web/app/assets/javascript/controllers/psicologosPerfilController.js
+++
@@ -1,8 +0,0 @@
-angular.module('Emozio').controller('PsicologosPerfilController', function(Psicologo, $scope, $routeParams){
-
-    /* Tomamos al psicologo indicado en los parametros */
-    Psicologo.GetById($routeParams.id).then(function(data){
-        $scope.psicologo=Object.values(data.data)[0];
-    });
-
-});
