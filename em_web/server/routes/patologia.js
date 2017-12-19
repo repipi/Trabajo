a489509af19f585cdccf2530f0a4dae0d768e24f
repipi <raquelgilmartinez@gmail.com>
2017-12-19 17:42:01 -0100

    Fin incremento 1 - Sólo versión em_web

---
+++ em_web/server/routes/patologia.js
@@ -0,0 +1,35 @@
+var Patologia = require('../models/patologia')
+
+module.exports = function(app) {
+
+    app.route('/patologias')
+    /* Obtiene patologias */
+        .get(function(req, res){
+
+        var patologia = new Patologia();
+        var promise = patologia.findAll();
+        promise.then(
+            function(data){
+                res.send(data);
+            },
+            function (error){
+                res.status(500).send({error: error});
+            }
+        );
+    });
+    
+    app.route('/patologias/preguntas')
+     .get(function(req, res){
+
+        var patologia = new Patologia();
+        var promise = patologia.findPreguntas();
+        promise.then(
+            function(data){
+                res.send(data);
+            },
+            function (error){
+                res.status(500).send({error: error});
+            }
+        );
+    });
+};
